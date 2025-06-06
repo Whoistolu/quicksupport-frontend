3dead5693439465466544d0a9ff0a0b68c252f36
Whoistolu <digittolu25@gmail.com>
2025-06-06 23:44:44 +0100

    Add React Router for client-side routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,6 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import TicketList from './pages/TicketList';
+import TicketDetail from './pages/TicketDetail';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -9,7 +12,12 @@ import client from './lib/apollo.js'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<TicketList />} />
+          <Route path="/tickets/:id" element={<TicketDetail />} />
+        </Routes>
+      </BrowserRouter>
     </ApolloProvider>
   </StrictMode>,
 )
